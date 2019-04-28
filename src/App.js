e45331e9751dea01f392a342f5a3848506a449e5
EUNJI-JEON <jej.career@gmail.com>
2019-04-28 22:08:57 +0900

    the component part is done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Movie from './Movie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  state = {
+
+
+  }
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({
+        movies: [
+          {
+              title:"Matrix",
+              poster:""
+        
+          },
+          {
+              title:"Full Metal jacket",
+              poster:""
+        
+          },
+          {
+              title:"Oldboy",
+              poster:""
+        
+          },
+          {
+              title:"Star-wars",
+              poster:""
+        
+          }
+        ]
+     
+
+      })
+    }, 1000)
+
+
+  }
+
+  _renderMovies = ()=>{
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index}/>
+    })
+
+    return movies
+
+    
+
+  }
+
+  render() {
+
+    return (
+      <div className="APP">
+  
+
+      
+      </div>
+    );
+  }
 }
 
 export default App;
